4b9bafc310d806255271badfb5cb74ccc9411e52
matheusolivr <contato.matheusoliv@gmail.com>
2023-06-19 11:08:41 -0300

    Cria o script responsavél por juntar apenas colunas passadas como parâmetros.

---
+++ script.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+cols = ['TX_NUM_EQUIPAMENTO', 'Num_Operacional', 'Ident_Equip_Def_ONS', 'Tensao_Nominal', 'Sigla_Subestacao']
+directory = r'C:\Users\Matheus\OneDrive\Nuvem\ELSC\PROJETOS\SGBDIT - CHESF - PREENCHIMENTO PLANILHAS\004-DB-CHESF'
+print("------------------ | Script iniciado com sucesso | ------------------")
+results  = pd.DataFrame()
+for i in os.listdir(directory):
+    file = os.path.join(directory,i)
+    df = pd.read_excel(file,usecols=cols,sheet_name = 0,skiprows=1)
+    results = pd.concat([results, df])
+    print("Os dados da planilha |"+i+"| foram carreagados com sucesso para planilha de relatório.")
+results.to_excel(os.path.join(directory,"results.xlsx"))
+print("Dados combinados salvos com sucesso!")
